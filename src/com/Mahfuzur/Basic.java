00105edbacc98f13edda3288fbd78d527ced1ee0
mahfuzur <mdmahfuazur7788@gamil.com>
2023-05-31 01:36:55 +0600

    Basic java if and while loop Temperature program

---
+++ src/com/Mahfuzur/Basic.java
@@ -0,0 +1,25 @@
+package com.Mahfuzur;
+
+public class Basic {
+    public static void main(String[] args) {
+//        if (true){
+//            System.out.println("True");
+//        }
+//
+//        int a = 10;
+//        if (a == 10){
+//            System.out.println( "a is true");
+//        }
+//
+       int count = 1;
+       while (count!= 5){
+           System.out.println(count);
+           count++;
+        }
+
+       //for loop
+        for (int i = 1;  i != 5; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/com/Mahfuzur/Temperature.java
@@ -0,0 +1,14 @@
+package com.Mahfuzur;
+
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number :");
+
+        float tempC = input.nextFloat();
+        float tempF = (tempC * 9/5) + 32;
+        System.out.println(tempF);
+    }
+}

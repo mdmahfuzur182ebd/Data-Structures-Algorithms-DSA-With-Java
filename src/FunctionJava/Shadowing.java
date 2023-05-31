9ecfcdfc5bcbbc160158b9c92582175380f91292
mahfuzur <mdmahfuazur7788@gamil.com>
2023-06-01 01:14:17 +0600

    Shadowing in java

--- src/FunctionJava/Shadowing.java
+++ src/FunctionJava/Shadowing.java
@@ -1,7 +1,17 @@
 package FunctionJava;
 
 public class Shadowing {
+    static int x = 900; //this will be shadowed at line 8
     public static void main(String[] args) {
+        System.out.println(x);
+        int x ; //the class variable at line 4 is shadowed by this
+      //  System.out.println(x); //scope will begin when value is initialised
+        x =49;
+        System.out.println(x);
+        fun();
+    }
 
+    static void fun(){
+        System.out.println(x);
     }
 }

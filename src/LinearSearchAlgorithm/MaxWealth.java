4385e61c29f96be59a63d6f0d9156a4797cf2d7c
mahfuzur <mdmahfuazur7788@gamil.com>
2023-06-15 17:00:00 +0600

    Max Wealth leetcode 1672

---
+++ src/LinearSearchAlgorithm/MaxWealth.java
@@ -0,0 +1,28 @@
+package LinearSearchAlgorithm;
+
+public class MaxWealth {
+    public static void main(String[] args) {
+       int[][] accounts = {
+               {1,2,3},
+               {3,2,1}
+       };
+        System.out.println(maximumWealth(accounts));
+    }
+
+    static  int maximumWealth(int[][] accounts) {
+        int ans = Integer.MIN_VALUE;
+        for (int person = 0; person <accounts.length ; person++) {
+            // When you start a new col, take a new sum for that row
+            int sum = 0;
+            for (int account = 0; account < accounts[person].length; account++) {
+                 sum += accounts[person][account];
+            }
+            // now we have sum of accounts of person
+            // check with overall ans
+            if (sum > ans){
+                ans = sum;
+            }
+        }
+        return ans;
+    }
+}

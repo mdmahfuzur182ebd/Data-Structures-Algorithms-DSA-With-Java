57e055bf8c9b211965d5a3e28c715c3eb1fd8095
mahfuzur <mdmahfuazur7788@gamil.com>
2023-06-17 00:55:28 +0600

    code for order-agnostic Binary Search

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -2,8 +2,8 @@ package BinarySearch;
 
 public class BinarySearch {
     public static void main(String[] args) {
-       int[] arr = {-18, -12, -4, 0, 2,3,4,15,16,22, 45};
-        System.out.println(BinarySearchProblem(arr, 15));
+       int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45};
+        System.out.println(BinarySearchProblem(arr, 18));
     }
 
 
@@ -12,7 +12,9 @@ public class BinarySearch {
     static  int BinarySearchProblem(int[] arr , int target){
          int start = 0;
          int end = arr.length - 1;
+
          while (start <= end){
+             // find the middle element
              int mid = start + (end-start) / 2;
              if (target < arr[mid] ){
                  end = mid -1;
@@ -20,6 +22,7 @@ public class BinarySearch {
                  start = mid + 1;
              }
              else {
+                 // ans found
                  return  mid;
              }
          }
